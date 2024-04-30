82ada3ea6db95d6f96186ae74ba2c70280d7a083
Isaac-Biss <isaacssozi98@gmail.com>
2024-05-01 00:32:14 +0300

    setting config

--- DjangoEcommerce/DjangoEcommerce/settings.py
+++ DjangoEcommerce/DjangoEcommerce/settings.py
@@ -116,7 +116,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+MEDIA_URL="/media/"
+MEDIA_ROOT=os.path.join(BASE_DIR,"media")
+print("MEDIA_ROOT::", MEDIA_ROOT)
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles')
+
+BASE_URL="http://127.0.0.1:8000"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
